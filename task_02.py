3a3049927e0ac297aad76c4598bc140524d9ea91
Artem Shihranov <artemshihranov7272@gmail.com>
2026-05-02 21:46:36 +0300

    Добавлено решение второго задания

---
+++ task_02.py
@@ -0,0 +1,35 @@
+'''
+Дан список list и числовой диапазон range. Разработайте метод coincidence(list,
+range) для определения элементов из массива list, значения которого входят в
+указанный диапазон range. Если не передан хотя бы один из параметров, то
+должен вернуться пустой массив.
+'''
+
+from typing import Iterable
+
+
+
+def coincidence(lst: Iterable[int | str | float | None] | None = None, 
+                rng: range | None = None
+                ) -> Iterable[int]:
+    
+    # проверяю, что переданы все аргументы
+    if not all((lst, rng)):
+        return []
+    
+    res = [] 
+    
+    # пробегаюсь по по списку и 
+    for obj in lst:
+        if isinstance(obj, (float, int)) and obj >= rng.start and obj < rng.stop:
+            res.append(obj)
+    
+    return res
+
+
+
+
+if __name__ == '__main__':
+    assert coincidence([1, 2, 3, 4, 5], range(3, 6)) == [3, 4, 5]
+    assert coincidence() == []
+    assert coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4)) == [1, 2, 2.5]
