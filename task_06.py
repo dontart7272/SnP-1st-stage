2b8877ef89de89065be3fffc7655f2c47f563fb3
Artem Shihranov <artemshihranov7272@gmail.com>
2026-09-20 18:59:54 +0300

    Исправлена ошибка в задании 6

--- task_06.py
+++ task_06.py
@@ -14,79 +14,76 @@ WrongNumberOfPlayersError
 • в иных случаях необходимо вернуть имя и ход победителя, если оба
 игрока походили одинаково - выигрывает первый игрок.
 '''
-from typing import List
-
-
 
 class WrongNumberOfPlayersError(ValueError):
     '''Класс исключения, вызывается при несоответствии количества игроков (2))'''
-    
-    def __init__(self, *args):
+
+    def __init__(self, *args) -> None:
         self.players: int = args[0]
-        
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"Число игроков отлично от 2 (Передано: self.players)" \
                if self.players \
                else "Число игроков отлично от 2"
-    
-    
+
+
 
 class NoSuchStrategyError(ValueError):
     '''Класс исключения, вызывается при использовании несуществующего хода'''
-    
-    def __init__(self, *args):
+
+    def __init__(self, *args) -> None:
         self.player: str = args[0]
         self.move: str = args[1]
-        
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"Передан несуществующий ход для игрока (Возможные ходы: P, S, R; Передано: self.move)" \
                 if self.player and self.move \
                 else "Число игроков отлично от 2 (Возможные ходы: P, S, R)"
-                
 
 
-# Словарь соответствия хода числовому значению              
+
+# Словарь соответствия хода числовому значению
 MOVE_INDEX = {
     'R': 0,
     'P': 1,
     'S': 2
-}              
-     
-                
+}
+
+
 def rps_game_winner(moves: List[List[str, str]]) -> str:
     '''
     Функция для определения победеителя в игре Камень, Ножницы, Бумага.
-    
+
     Логика:
     Каждому из возможных ходов присваивается числовое значение
     Камень - 0
     Бумага - 1
     ножницы - 2
-    
-    При проверке победителя вычитаем из числа, которое соответствует ходу игрока 2 число, 
+
+    При проверке победителя вычитаем из числа, которое соответствует ходу игрока 2 число,
     которое соответствует ходу игрока 1. Вычисляем остаток от деления на 3 из полученной разности.
-    
+
     В итоге получем число, которое соответствует номеру победившего игрока
     либо 0 - тоже соответствует первому игроку.
     '''
-    
+
     # Проверяю, что игроков не более двух
     if len(moves) > 2:
         raise WrongNumberOfPlayersError(len(moves))
-    
+
     # Проверяю, что каждый игрок выбрал существующий вариант хода
     lst = [move in 'PRS' for player, move in moves]
-    
+
     if not all(lst):
         raise NoSuchStrategyError(*moves[lst.index(False)])
-    
+
     # Основная логика проверки, которая описана в шапке
     winner = (MOVE_INDEX[moves[0][1]] - MOVE_INDEX[moves[1][1]]) % 3
-    
+
     return f'{moves[0][0]} {moves[0][1]}' if winner in (0, 1) \
         else f'{moves[1][0]} {moves[1][1]}'
-        
-        
+
+
 
 
 if __name__ == '__main__':
@@ -98,10 +95,7 @@ if __name__ == '__main__':
         rps_game_winner([['player1', 'P'], ['player2', 'A']])
     except NoSuchStrategyError:
         print('Тест 2 пройден')
-    
+
     assert rps_game_winner([['player1', 'P'], ['player2', 'S']]) == 'player2 S'; print('Тест 3 пройден')
     assert rps_game_winner([['player1', 'P'], ['player2', 'P']]) == 'player1 P'; print('Тест 4 пройден')
     print('Все тесты пройдены успешно')
-
-
-        
