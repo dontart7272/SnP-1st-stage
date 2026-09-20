112d99df0b9ae5110f48c8186778b04ccebafa04
Artem Shihranov <artemshihranov7272@gmail.com>
2026-09-20 19:00:19 +0300

    Добавлено решение задания 7

--- task_07.py
+++ task_07.py
@@ -7,10 +7,22 @@
 значения при определении анаграмм.
 '''
 
-from typing import List
 
+def combine_anagrams(words_array: list[str]) -> list[list[str]]:
 
-def combine_anagrams(words_array: List[str]) -> List[List[str]]:
-    
+    # Словарь для удобства хранения данных
+    groups: dict[str, list[str]] = {}
+
+    # перебираем все элементы массива
     for word in words_array:
-        
+        # Ключ слловаря - отсортированная по возрпастанию строка
+        key = ''.join(sorted(word.lower()))
+        # Если ключ уже есть, то, добавляем слово в список связанный с ключем, есмли нет, то создаем новую пару ключ-значение
+        groups.setdefault(key, []).append(word)
+
+    return sorted(groups.values())
+
+
+
+if __name__ == '__main__':
+    assert combine_anagrams(["cars", "for", "potatoes", "racs", "four", "scar", "creams", "scream"]) == sorted([ ["cars", "racs", "scar"], ["four"], ["for"], ["potatoes"], ["creams", "scream"] ])
